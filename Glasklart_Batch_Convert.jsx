9f856264651056a396ec039c51064cd584431527
dreamnet <webmaster@dreamnet.at>
2015-02-22 11:25:46 +0100

    Update Batch Convert script

--- Glasklart_Batch_Convert.jsx
+++ Glasklart_Batch_Convert.jsx
@@ -15,19 +15,17 @@
 //          -> 03_Glasklart_Icon_Template_@3x.psd
 //
 // take care that this script deletes the input files after converting them to Glasklart icons without any prompting !!!
-// if you don't want his, simply comment out line #74 (// sourceFile.remove();) <----------------------------------- !!!
+// if you don't want his, simply comment out line #51 (// sourceFile.remove();) <----------------------------------- !!!
 //
-// written by @dreamnet 12-22-14
+// written by @dreamnet 12-22-14 - last overworked on 02-22-15
 
-#target photoshop // opening Photoshop
-app.bringToFront(); // bring Photoshop to front
+#target photoshop
+app.bringToFront();
 
 // the base folder, normally you only need to change this to wherever your source files are (without ending slash!)
 var baseFolder = 'D:/Users/Kirchi/Desktop/Glasklart PScripts'; 
 
-// no changes should be done below this line unless You know what you are doing! ----------------------------------
-
-var inputFolder = Folder(baseFolder+'/input'); // the folder witch contains the 120x120px source files
+var inputFolder = Folder(baseFolder+'/input'); // the folder witch contains the 1024x1024px source files
 var outputFolder = Folder(baseFolder+'/output'); // the folder where the finished Glasklart icons are stored
 
 // paths to template files
@@ -35,49 +33,50 @@ var templateFile1 = File(baseFolder+'/templates/01_Glasklart_Icon_Template_@2x.p
 var templateFile2 = File(baseFolder+'/templates/02_Glasklart_Icon_Template_~iPad@2x.psd');
 var templateFile3 = File(baseFolder+'/templates/03_Glasklart_Icon_Template_@3x.psd');
 
-var templateDoc = open(templateFile1);
-
 var fileList = inputFolder.getFiles('*.png'); // getting all .png's in input folder
 
 for(var i=0; i<fileList.length; i++) { // loop trough input files
     
     // set path to source file end open it
     var sourceFile = File(fileList[i]);
-    var sourceDoc = open(sourceFile);
 
     // get sourceDoc name (and remove file extension)
-    var docName = sourceDoc.name;
+    var docName = sourceFile.name;
     docName = docName.replace(/(?:\.[^.]*$|$)/, '');
 
-    // rename layer; duplicate to new document
-    var layer = sourceDoc.activeLayer;
-    layer.name = docName;
-    activeDocument.selection.selectAll();
-    sourceDoc.artLayers[0].duplicate(templateDoc, ElementPlacement.PLACEATBEGINNING);
-
-    sourceDoc.close(SaveOptions.DONOTSAVECHANGES); // close sourceDoc
-
-    var layer = templateDoc.activeLayer;
-
-    // make a 120x120px selection and move it to center (can't translate layer because of the transparent pixels)
-    activeDocument.selection.select([[0,0],[0,120],[120,120],[120,0]]);
-    activeDocument.selection.translate(30,30);
-    activeDocument.selection.deselect();
-
-    layerGroup = activeDocument.layerSets.getByName("Sample Icons");
-    layerCopy = layerGroup.layers.getByName("com.atebits.Tweetie2");
-    transferEffects(layerCopy, layer); // copy Glasklart style from sample icon to inserted layer
-
-    // save the file
-    saveFile(pathPart, fileName);
+    generateGlasklartIcon(78, 21, sourceFile, templateFile1, outputFolder+"/@2x", docName+"@2x");           // generates 120px Glasklart icon (@2x)      | you can comment
+    generateGlasklartIcon(100, 26, sourceFile, templateFile2, outputFolder+"/iPad@2x", docName+"~iPad@2x"); // generates 152px Glasklart icon (~ipad@2x) | out what you
+    generateGlasklartIcon(120, 30, sourceFile, templateFile3, outputFolder+"/@3x", docName+"@3x");          // generates 180px Glasklart icon (@3x)      | don't need
     
     sourceFile.remove(); // delete source file from disk
 
 }
 
-templateDoc.close(SaveOptions.DONOTSAVECHANGES); // all finished, close template doc
+// no changes should be done below this line ----------------------------------------------------------------------
 
 // functions ------------------------------------------------------------------------------------------------------
+
+// function for generating a Glasklart icon
+function generateGlasklartIcon(size,offSet,sourceFile,templateFile,outputFolder,fileName) {
+    var templateDoc = open(templateFile); // open template
+    template = app.activeDocument; // reference template
+    if(sourceFile != null) { var sourceDoc = open(sourceFile); } // open source file
+    source = app.activeDocument; // reference source
+    source.resizeImage(size,size,null,ResampleMethod.BICUBIC); // resize source file to given size
+    source.selection.selectAll(); // select whole source document
+    source.artLayers[0].duplicate(template, ElementPlacement.PLACEATBEGINNING); // duplicate layer to template
+    source.close(SaveOptions.DONOTSAVECHANGES); // closing source file, not needed anymore
+    app.activeDocument = template; // switch to template file
+    var layer = template.activeLayer; // reference layer
+    template.selection.select([[0,0],[0,size],[size,size],[size,0]]); // selecting the whole layer
+    template.selection.translate(offSet,offSet); // moving selection (cant move layer, bacause Photoshop don't sees transparent pixels)
+    template.selection.deselect(); // remove selection
+    layerGroup = template.layerSets.getByName("Sample Icons");
+    layerCopy = layerGroup.layers.getByName("com.atebits.Tweetie2");
+    transferEffects(layerCopy, layer); // copy Glasklart style from sample icon to inserted layer
+    saveFile(outputFolder, fileName); // save file to above set output folder
+    templateDoc.close(SaveOptions.DONOTSAVECHANGES); // closing template
+}
 
 // function for save finished Glasklart icon (save for web)
 function saveFile(pathPart, fileName) {
@@ -86,7 +85,7 @@ function saveFile(pathPart, fileName) {
     opts.format = SaveDocumentType.PNG;
     opts.PNG8 = false;
     opts.quality = 100;
-    outputFile = new File(outputFolder+'/'+pathPart+'/'+fileName+'.png');
+    outputFile = new File(pathPart+'/'+fileName+'.png');
     app.activeDocument.exportDocument(outputFile, ExportType.SAVEFORWEB, opts);
 }
 
