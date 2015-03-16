142677ac6d7d20b76ffb341cdd20bf8ad3030c3e
dreamnet <webmaster@dreamnet.at>
2015-03-16 23:50:47 +0100

    Small code cleanup

--- Glasklart_Generate_Icons_Smart.jsx
+++ Glasklart_Generate_Icons_Smart.jsx
@@ -11,7 +11,6 @@ var templateFolder = 'D:/Users/Kirchi/Desktop/Glasklart PScripts'; // the folder
 var outputFolder = 'D/Users/Kirchi/Desktop'; // The output folder where the finished icons will be saved.
 // end editable vars ----------------------------------------------------------------------------------------------
 
-// no changes should be done below this line unless You know what you are doing! ----------------------------------
 // Show dialog for the 1024x1024px png source file
 var sourceFile = File.openDialog ("Please select the 1024x1024px Glasklart PNG source file", "*.png", false)
 // set paths to template files
@@ -19,9 +18,10 @@ var templateFile1 = File(templateFolder+'/templates/01_Glasklart_Icon_Template_@
 var templateFile2 = File(templateFolder+'/templates/02_Glasklart_Icon_Template_~iPad@2x_Smart.psd');
 var templateFile3 = File(templateFolder+'/templates/03_Glasklart_Icon_Template_@3x_Smart.psd');
 // generating the icons
-generateGlasklartIconSmart(templateFile1,sourceFile,outputFolder,"@2x"); // generates 120px Glasklart icon (@2x)           | comment out
-generateGlasklartIconSmart(templateFile2,sourceFile,outputFolder,"~iPad@2x"); // generates 152px Glasklart icon (~ipad@2x) | what you
-generateGlasklartIconSmart(templateFile3,sourceFile,outputFolder,"@3x"); // generates 180px Glasklart icon (@3x)           | don't need
+generateGlasklartIconSmart(templateFile1,sourceFile,outputFolder,"@2x"); // generates @2x Glasklart icon       | comment out
+generateGlasklartIconSmart(templateFile2,sourceFile,outputFolder,"~iPad@2x"); // generates iPad Glasklart icon | what you
+generateGlasklartIconSmart(templateFile3,sourceFile,outputFolder,"@3x"); // generates @3x Glasklart icon       | don't need
+// end of script --------------------------------------------------------------------------------------------------
 
 // functions ------------------------------------------------------------------------------------------------------
 function generateGlasklartIconSmart(templateFile,sourceFile,outputFolder,fileName) {
@@ -34,11 +34,11 @@ function generateGlasklartIconSmart(templateFile,sourceFile,outputFolder,fileNam
     templateDoc.close(SaveOptions.DONOTSAVECHANGES); // closing template
 }
 // function to replace content of a smart object
-function replaceSO(newFile) {
+function replaceSO(sourceFile) {
     var id = stringIDToTypeID( "placedLayerReplaceContents" );
     var desc = new ActionDescriptor();
     var idn = charIDToTypeID("null");
-    desc.putPath( idn, new File(newFile));
+    desc.putPath( idn, new File(sourceFile));
     var idp = charIDToTypeID("PgNm");
     desc.putInteger(idp, 1);
     executeAction(id, desc, DialogModes.NO);
@@ -54,4 +54,3 @@ function saveFile(outputFolder, fileName) {
     outputFile = new File('/'+outputFolder+'/'+fileName+'.png');
     app.activeDocument.exportDocument(outputFile, ExportType.SAVEFORWEB, opts);    
 }
-// end of script --------------------------------------------------------------------------------------------------
